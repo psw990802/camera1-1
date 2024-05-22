a688719eeb375191a9aa22c6ce5fb0f1a679bdf3
psw990802 <133945363+psw990802@users.noreply.github.com>
2024-05-21 18:02:58 -0700

    Add files via upload

---
+++ camera1-1/src/sub_wsl.cpp
@@ -0,0 +1,32 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
+#include "opencv2/opencv.hpp"
+#include <memory>
+#include <functional>
+#include <iostream>
+using std::placeholders::_1;
+  
+void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
+{
+    cv::Mat frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_COLOR);
+    cv::Mat gray,bin;
+    cv::cvtColor(frame,gray,cv::COLOR_BGR2GRAY);
+    cv::threshold(gray,bin,0,255,cv::THRESH_OTSU|cv::THRESH_BINARY);
+    cv::imshow("wsl_frame",frame);
+    cv::imshow("wsl_gray",gray);
+    cv::imshow("wsl_bin",bin);
+    cv::waitKey(1);
+    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
+}
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("camsub_wsl");
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+    std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
+    fn = std::bind(mysub_callback, node, _1);
+    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed",qos_profile,fn);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
